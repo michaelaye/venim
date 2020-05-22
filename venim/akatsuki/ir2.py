ea59b28f5e7961675a190976a73842dc8269182e
K.-Michael Aye <kmichael.aye@gmail.com>
2020-05-21 20:51:40 -0600

    Add tons of documention in notebooks
    add urlpath requirement in setup.py
    fix some use-ability bugs with orbit

--- venim/akatsuki/ir2.py
+++ venim/akatsuki/ir2.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from pathlib import Path
 from urllib.request import urlretrieve
 
@@ -82,8 +81,49 @@ def download_list_of_urls(savedir, urllist, force=False, test=False, verbose=Fal
 
 
 class Downloader:
-    def __init__(self, level="calibrated"):
+    def __init__(self, level="calibrated", orbit=None):
+        """Downloader class for Akatsuki IR2 data.
+
+        The orbit can be set at creation of a Downloader object, or set later.
+
+        Examples
+        --------
+        >>> dl = Downloader(orbit=25)
+        >>> dl.orbit
+        25
+
+        or
+        >>> dl = Dowloader()
+        >>> dl.orbit = 25
+        >>> dl.orbit
+        25
+
+        Parameters
+        ----------
+        level : {'raw','calibrated','geom}
+            Akatsuki data kind to work with
+        orbit : str, int
+            The Akatsuki orbit. Both an integer and the revolution string 'rxxxx' is allowed.
+        """
         self._level = level
+        self.orbit = orbit
+
+    @property
+    def orbit(self):
+        """str: Akatsuki's revolution string with format 'rxxxx'.
+
+        The setter method takes both the int and str version as new value.
+        """
+        return self._orbit
+
+    @orbit.setter
+    def orbit(self, new_orbit):
+        if new_orbit is None:
+            self._orbit = None
+            return
+        if not str(new_orbit).startswith("r"):
+            new_orbit = "r" + str(new_orbit).zfill(4)
+        self._orbit = new_orbit
 
     @property
     def level(self):
@@ -101,8 +141,12 @@ class Downloader:
             )
         self._level = new_level
 
-    def get_orbit_filelist(self, orbit):
-        df = get_rev_filelist(orbit, self.level)
+    def get_orbit_filelist(self, orbit=None):
+        if orbit is None:
+            orbit = self.orbit
+        else:
+            self.orbit = orbit
+        df = get_rev_filelist(self.orbit, self.level)
         self.filelist = df
         return df
 
@@ -111,20 +155,62 @@ class Downloader:
         pm = IR2PathManager()
         return pm.instr_savedir / self.level
 
-    def urls_for_orbit(self, orbit):
-        df = self.get_orbit_filelist(orbit)
-        urls = level_urls[self.level] / orbit / df.filename
+    def urls_for_orbit(self, orbit=None):
+        if orbit is None:
+            orbit = self.orbit
+        else:
+            self.orbit = orbit
+        df = self.get_orbit_filelist(self.orbit)
+        urls = level_urls[self.level] / self.orbit / df.filename
         return urls
 
-    def download_orbit_files(self, orbit, only_fits=True, **kwargs):
+    def download_orbit_files(self, orbit=None, only_fits=True, **kwargs):
+        if orbit is None:
+            orbit = self.orbit
+        else:
+            self.orbit = orbit
         urls = self.urls_for_orbit(orbit)
         if only_fits:
             urls = [url for url in urls if url.suffix.endswith(".fit")]
-        orbit_savedir = self.savedir / orbit
+        orbit_savedir = self.savedir / self.orbit
         download_list_of_urls(orbit_savedir, urls, **kwargs)
 
 
 class IR2FileName:
+    """Class to dismantle an IR2FileName.
+
+    Each splittable part of the filename is reachable as a class attribute, and the
+    datestr and timestr data has been used to create a `pandas.Timestamp`, which can
+    easily be converted back to a datetime object.
+
+    Example
+    -------
+    >>> irfn = IR2FileName('ir2_20160829_201008_202_l2b_v10.fit')
+    >>> irfn.wavelength
+    202
+    >>> irfn.datetime
+    Timestamp('2016-08-29 20:10:08')
+
+    Parameters
+    ----------
+    fname : str
+        Akatsuki filename in the format `ir2_20160829_201008_202_l2b_v10.fit`
+
+    Attributes
+    ----------
+    tokens
+    instr
+    datestr
+    date
+    timestr
+    time
+    datetime
+    wavelength
+    in_micron
+    level
+    version
+    """
+
     def __init__(self, fname):
         self.name = Path(fname).name
 
@@ -161,6 +247,10 @@ class IR2FileName:
         return self.tokens[3]
 
     @property
+    def in_micron(self):
+        return int(self.wavelength) / 100
+
+    @property
     def level(self):
         return self.tokens[4]
 
@@ -168,16 +258,41 @@ class IR2FileName:
     def version(self):
         return self.tokens[5]
 
+    def __str__(self):
+        s = f"IR2 FileName {self.name}\n"
+        s += f"DateTime: {self.datetime}\n"
+        s += f"Wavelength: {self.in_micron} micron.\n"
+        s += f"Level: {self.level}\n"
+        s += f"Version: {self.version}\n"
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class IR2PathManager(PathManager):
     """Class to manage files for Akatsuki IR2 data.
 
+    Offers method `list_files_for_orbit` to get a list of found files per orbit in
+    form of a pandas.DataFrame.
     Parameters
     ----------
     level : {'raw', 'calibrated', 'geom'}
         Which IR2 data to choose from.
+    orbit : int, str; optional
+        Akatsuki orbit, both revolution string rxxxx and integer number is allowed.
     full_paths : bool, optional
         Switch to show full paths on disk.
+
+    Attributes
+    ----------
+    orbit
+    savedir
+
+    Methods
+    -------
+    list_files_for_orbit(orbit=None, full_paths=None)
+        Get a file listing
     """
 
     def __init__(self, level="calibrated", orbit=None, full_paths=False):
@@ -188,6 +303,7 @@ class IR2PathManager(PathManager):
 
     @property
     def orbit(self):
+        """str: Akatsuki orbit in format of revolution string 'rxxxx'."""
         return self._orbit
 
     @orbit.setter
@@ -200,6 +316,20 @@ class IR2PathManager(PathManager):
         self._orbit = new_orbit
 
     def list_files_for_orbit(self, orbit=None, full_paths=None):
+        """Get a file listing as a pandas.DataFrame.
+
+        Parameters
+        ----------
+        orbit : str, int
+            Akatsuki orbit, either as rxxxx string or integer.
+        full_paths : bool
+            Switch to show either truncated or pull paths.
+
+        Returns
+        -------
+        pandas.DataFrame
+            The Dataframe lists filenames and the datetimes for the files.
+        """
         if orbit is None:
             orbit = self.orbit
         if full_paths is None:
@@ -214,6 +344,7 @@ class IR2PathManager(PathManager):
         return df
 
     def get_path(self, index):
+        ""
         filelist = self.list_files_for_orbit(full_paths=True)
         return filelist.filename.iloc[index]
 
@@ -224,15 +355,3 @@ class IR2PathManager(PathManager):
     @property
     def savedir(self):
         return self.instr_savedir / self.level
-
-    @property
-    def raw_savedir(self):
-        return self.instr_savedir / "raw"
-
-    @property
-    def calibrated_savedir(self):
-        return self.instr_savedir / "calibrated"
-
-    @property
-    def geometry_savedir(self):
-        return self.instr_savedir / "geometry"
