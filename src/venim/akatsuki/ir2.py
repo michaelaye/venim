c32d0fb4fbd9949b76a26a756b0c0f91c9675f33
Michael Aye <kmichael.aye@gmail.com>
2020-06-18 08:31:13 -0600

    fix(data): add missing Image class properties

--- src/venim/akatsuki/ir2.py
+++ src/venim/akatsuki/ir2.py
@@ -7,27 +7,16 @@ import webbrowser
 from pathlib import Path
 from urllib.request import urlretrieve
 
-import circle_fit as cf
-import holoviews as hv
-import numpy as np
 import pandas as pd
 import pvl
 import requests
 from astropy.io import fits
-from holoviews import opts
-from skimage.exposure import equalize_adapthist as equalize
-from skimage.exposure import rescale_intensity
 from tqdm.auto import tqdm
 from urlpath import URL
 
 from ..config import config
+from ..image import Image
 from ..pathmanager import PathManager
-
-hv.extension("bokeh")
-opts.defaults(
-    opts.Image(tools=["hover"], cmap="gray"),
-    opts.Points(color="red", marker="x", size=20),
-)
 
 storage_root = Path(config["venim_path"]).expanduser()
 storage_root.mkdir(exist_ok=True, parents=True)
@@ -524,89 +513,11 @@ def open_fits_dic():
     open_doc_url("fits_dic")
 
 
-class Image:
-    def __init__(self, id):
-        p = Path(id)
+class IR2Image(Image):
+    def __init__(self, p_or_id):
+        p = Path(p_or_id)
         if not p.is_absolute():
-            p = get_file_path(id)
-        self.path = p
-        self.data, self.header = fits.getdata(p, header=True)
-
-    @property
-    def name(self):
-        return self.path.name
-
-    @property
-    def wavelength(self):
-        return IR2FileName(self.name).wavelength
-
-    @property
-    def exposure(self):
-        return self.header["EXPOSURE"]
-
-    @property
-    def imagetime(self):
-        return IR2FileName(self.name).datetime
-
-    @property
-    def full_frame(self):
-        newimg = np.zeros((1024, 1024))
-        ll_row = self.header["P_POSLLY"] - 1
-        ll_col = self.header["P_POSLLX"] - 1
-        ur_row = self.header["P_POSURY"]
-        ur_col = self.header["P_POSURX"]
-        newimg[ll_row:ur_row, ll_col:ur_col] = self.data
-        return newimg
-
-    @property
-    def rescaled(self):
-        return rescale_intensity(self.full_frame, out_range="float")
-
-    @property
-    def equalized(self):
-        return equalize(self.rescaled)
-
-    def plot(self, sqrt=False, pmin=1, pmax=99, with_fit=False):
-        data = self.full_frame
-        data[data < 0] = np.nan
-        if sqrt:
-            data = np.sqrt(data)
-        vmin, vmax = np.percentile(data[~np.isnan(data)], (pmin, pmax))
-        if sqrt:
-            data = np.sqrt(data)
-        img = hv.Image(
-            (np.arange(1024), np.arange(1024), data), label=self.plot_title
-        ).opts(clim=(vmin, vmax), frame_height=500, frame_width=500,)
-        if with_fit:
-            return img * self.circle_fit
+            path = get_file_path(p_or_id)
         else:
-            return img
-
-    @property
-    def plot_title(self):
-        t = f"{self.imagetime}, Filter: {self.wavelength}, Exp: {self.exposure} s"
-        return t
-
-    def plot_equalized(self):
-        return hv.Raster(self.equalized).redim.range(z=(0, None))
-
-    def annotate(self):
-        points = hv.Points([]).opts(width=500, height=500, padding=0, responsive=False)
-        self.annotator = hv.annotate.instance()
-        layout = self.annotator(
-            self.plot().opts(frame_height=400, frame_width=400,) * points,
-            name="Limb Points",
-        )
-        return layout
-
-    @property
-    def circle_fit(self):
-        xCtr, yCtr, r, _ = cf.least_squares_circle(self.points_data.values)
-        return hv.Ellipse(xCtr, yCtr, 2 * r).opts(color="red")
-
-    @property
-    def points_data(self):
-        try:
-            return self.annotator.annotated.dframe()
-        except AttributeError:
-            return pd.DataFrame()
+            path = p_or_id
+        super().__init__(path)
